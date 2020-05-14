2116445c697a7f59755535ad3c4750f65315720d
v1nc+ <git@reckendrees.systems>
2020-05-15 00:38:23 +0200

    add secrets example

---
+++ sources/secrets.example.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+SECRETS = {
+
+	# Bot Token (get it from @BotFather) 
+    "TOKEN": "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+
+    #API hash from my.telegram.org
+    "API_HASH" : "",
+
+    #API ID from my.telegram.org
+	"API_ID" : 1234,
+
+	#Name for the API account
+	"API_NAME": ""
+}
