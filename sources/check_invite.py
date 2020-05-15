c242b10b79ae6b37f00835c989cd24193b3adb84
v1nc <github@reckendrees.systems>
2020-05-15 18:10:55 +0200

    fix typo

--- sources/check_invite.py
+++ sources/check_invite.py
@@ -4,19 +4,19 @@ import sys
 try:
 	from secrets import SECRETS
 except Exception as e:
-	print("not")
+	print("secrets")
 	exit(1)
 if len(sys.argv) > 1:
 	try:
 		with TelegramClient(SECRETS["API_NAME"], SECRETS["API_ID"], SECRETS["API_HASH"]) as client:
-	    	result = client(functions.messages.CheckChatInviteRequest(hash=sys.argv[1]))
-	    	print("valid")
+			result = client(functions.messages.CheckChatInviteRequest(hash=sys.argv[1]))
+			print("valid")
 	except InviteHashExpiredError as ex:
-	    	print("not")
-	    	sys.exit(1)
+			print("not")
+			sys.exit(1)
 	except Exception as e:
 			print("telethon error")
-	    	sys.exit(1)
+			sys.exit(1)
 else:
 	print("please specify invite hash!")
 	sys.exit(1)
