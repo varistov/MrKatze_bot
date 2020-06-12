a9e185142221e8c11c23a53f45ff29b3e73fbd2d
v1nc <github@reckendrees.systems>
2020-06-12 13:42:01 +0200

    add message_to_html and use it for notes

--- sources/butter_bot.py
+++ sources/butter_bot.py
@@ -467,8 +467,9 @@ def send_command_list(bot,update):
 
 def send_to_owner(bot,chat_id, message):
 	try:
-		printts("[{}]: {}".format(chat_id,traceback.format_exc()))
-		bot.send_message(CONST["OWNER"],TEXT["EN"]["OWNER_ERROR_MSG"].format(str(message),traceback.format_exc()),parse_mode=ParseMode.HTML)
+		error = traceback.format_exc()
+		printts("[{}]: {}".format(chat_id,error))
+		bot.send_message(CONST["OWNER"],TEXT["EN"]["OWNER_ERROR_MSG"].format(str(message),error),parse_mode=ParseMode.HTML)
 	except Exception:
 		printts("[{}]: {}".format(chat_id,traceback.format_exc()))
 
@@ -513,6 +514,33 @@ def delete_if_muted(bot,update):
 			bot.delete_message(chat_id,msg_id)
 			return True
 	return False
+
+def message_to_html(og_text,entities,cmd_offset=0):
+	text = og_text
+	entity_types = {
+		"bold" : "b",
+		"italic" : "i",
+		"underline" : "u",
+		"strikethrough" : "s",
+		"code" : "code",
+		"pre": "pre",
+	} 
+	counter = 0
+	for e in entities:
+		if e.type in entity_types:
+			tag = entity_types[e.type]
+			text = text[:e.offset+counter-cmd_offset]+"<"+tag+">"+text[e.offset+counter-cmd_offset:e.offset+e.length+counter-cmd_offset]+"</"+tag+">"+text[e.offset+counter+e.length-cmd_offset:]
+			counter +=5+(len(tag)*2)
+		elif e.type == "text_link" and len(e.url) > 0:
+			url = e.url
+			text = text[:e.offset+counter-cmd_offset]+"<a href='{}'>".format(url)+text[e.offset+counter-cmd_offset:e.offset+e.length+counter-cmd_offset]+"</a>"+text[e.offset+counter+e.length-cmd_offset:]
+			counter+=15+len(url)
+		elif e.type == "text_mention":
+			user = e.user 
+			url = "tg://user?id={}".format(user["id"])
+			text = text[:e.offset+counter-cmd_offset]+"<a href='{}'>".format(url)+text[e.offset+counter-cmd_offset:e.offset+e.length+counter-cmd_offset]+"</a>"+text[e.offset+counter+e.length-cmd_offset:]
+			counter+=15+len(url)
+	return text
 ####################################################################################################
 
 ### JSON chat config file functions ###
@@ -999,7 +1027,7 @@ def msg_notext(update: Update, context: CallbackContext):
 	try:
 		bot = context.bot
 		if delete_if_muted(bot,update):
-                        return
+						return
 		# Check for normal or edited message
 		msg = getattr(update, "message", None)
 		if msg is None:
@@ -1796,8 +1824,9 @@ def cmd_add_trigger(update: Update, context: CallbackContext):
 		if allow_command:
 			if len(args) >= 2:
 				name = args[0]
-				message = " ".join(args[1:])
-				message = message.replace("<br>","\n")
+				message=" ".join(update.message.text.split(" ")[2:])
+				offset = len(update.message.text)-len(message)
+				message = message_to_html(message,update.message.entities,offset)
 				trigger_list = get_chat_config(chat_id,"Trigger_List")
 				trigger_list[name]=message
 				save_config_property(chat_id, "Trigger_List",trigger_list)
@@ -2458,6 +2487,7 @@ def cmd_info(update: Update, context: CallbackContext):
 		if delete_if_muted(bot,update):
 			return
 		msg =  update.message
+
 		chat_id = update.message.chat_id
 		user_id = update.message.from_user.id
 		chat_type = update.message.chat.type
